37c5fb75a2a446709a573042fa6e252c75efd5b1
alekse1f <67468043+alekse1f@users.noreply.github.com>
2021-03-14 20:53:04 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable (n) {
+    const numbers = [
+        "",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "ninety",
+    ];
+    const numbersH = [
+        "",
+        "one hundred ",
+        "two hundred ",
+        "three hundred ",
+        "four hundred ",
+        "five hundred ",
+        "six hundred ",
+        "seven hundred ",
+        "eight hundred ",
+        "nine hundred ",
+    ];
+    const numbersT = [
+        "",
+        "",
+        "twenty ",
+        "thirty ",
+        "forty ",
+        "fifty ",
+        "sixty ",
+        "seventy ",
+        "eighty ",
+        "ninety ",
+    ];
+    const f = n % 100;
+    let result = numbersH[Math.trunc(n / 100)];
+    if (f < 20) {
+        result += numbers[f];
+    } else {
+        result += numbersT[Math.trunc(f / 10)] + numbers[f % 10];
+    }
+    return result[result.length - 1] == " " ? result.slice(0, -1) : result;
 }
